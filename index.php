56c5d8f0f6472ac9606c6299ea85ed379d971c69
Luis Feliz <alfonsofeliz@gmail.com>
2014-03-07 16:09:21 -0400

    Agregado los menu
    Agregado el menu y parte del css

---
+++ index.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html>
+	<head>
+	<link rel="stylesheet" href="css/style.css">
+		<title>Inicio</title>
+
+	</head>
+<body>
+ 	<div id="contenedor"> 
+ 	<header>
+ 		<nav>
+ 		<ul>
+ 			<li><a href="#">Inicio</a></li>
+ 			<li><a href="#">Facturaci&oacuten</a></li>
+ 			<li><a href="#">Inventario</a></li>
+ 			<li><a href="#">Clientes</a></li>
+ 		</ul>
+
+ 			
+ 		</nav>
+ 	</header>
+ 	<section id="lado">
+ 	
+ 		
+ 	</section>
+ 	<section id="central"></section>
+ 	
+ 	<footer>
+ 		
+ 	</footer>
+
+	</div>
+</body>
+
+</html>
